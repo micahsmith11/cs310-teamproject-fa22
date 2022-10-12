70f9642c827b042a8a6b1dad66f402a648446932
Austin Rush <arush2@stu.jsu.edu>
2022-10-12 11:49:53 -0500

    Removed comments to clean code

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.jsu.mcis.cs310.tas_fa22.dao;
 
-/**
- *
- * @author acrus
- */
 public class ShiftDAO {
     
 }
