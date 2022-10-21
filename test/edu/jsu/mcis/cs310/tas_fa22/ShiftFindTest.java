6d7309c8e9d4639f0031efbfeb5d65edcfa5752f
JJones1038 <67795967+JJones1038@users.noreply.github.com>
2022-10-21 11:54:09 -0500

    Added extra test to ShiftFindTest

--- test/edu/jsu/mcis/cs310/tas_fa22/ShiftFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa22/ShiftFindTest.java
@@ -25,12 +25,14 @@ public class ShiftFindTest {
         Shift s1 = shiftDAO.find(1);
         Shift s2 = shiftDAO.find(2);
         Shift s3 = shiftDAO.find(3);
+        Shift s4 = shiftDAO.find(4);
 
         /* Compare to Expected Values */
         
         assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
         assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s2.toString());
         assertEquals("Shift 1 Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)", s3.toString());
+        assertEquals("Shift 3: 22:30 - 07:00 (-930 minutes); Lunch: 02:30 - 03:00 (30 minutes)", s4.toString());
 
     }
 
