acdd426f28edc48e46e0ffe4b9e898bb8f864094
Austin Rush <arush2@stu.jsu.edu>
2022-11-04 11:34:20 -0500

    Working on DAOUtility

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOUtility.java
@@ -12,8 +12,40 @@ import org.json.simple.*;
  * Utility class for DAOs.  This is a final, non-constructable class containing
  * common DAO logic and other repeated and/or standardized code, refactored into
  * individual static methods.
- * 
+ *
  */
 public final class DAOUtility {
+    private static Object ArrayList;
+    private static String punchData;
+    Punch punch;
 
+    private static class string {
+
+        public string() {
+        }
+    }
+    
+    public static calculateTotalMinutes(ArrayList<Punch> dailypunchlist, 
+            Shift shift){
+
+        
+        
+    } 
+     public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+        // Create ArrayList Object 
+        ArrayList<HashMap<string, string = "" >> jsonData;
+
+        // Create HashMap Object (one for every Punch!) 
+        HashMap<string, string = "" > punchData = new HashMap<>();
+
+        // Add Punch Data to HashMap
+        punchData.put("id", String.valueOf(punch.getId));
+        punchData.put("badgeid", String.valueOf(punch.getBadgeid));
+        punchData.put("terminalid", String.valueOf(punch.getTerminalid));
+        // finish with remaining Punch data 
+        
+        // Append HashMap to ArrayList
+        jsonData.add(punchData);
+
+        String json = JSONValue.toJSONString(jsonData);
 }
