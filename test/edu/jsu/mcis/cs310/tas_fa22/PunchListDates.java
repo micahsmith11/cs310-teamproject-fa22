3fac7776b86950d21e1ea63ecccd47658008a1d5
garya <garya@10.80.31.48>
2022-11-18 12:45:33 -0600

    Added Test for PunchDAO Rang of Dates Method

--- test/edu/jsu/mcis/cs310/tas_fa22/PunchListDates.java
+++ test/edu/jsu/mcis/cs310/tas_fa22/PunchListDates.java
@@ -28,7 +28,7 @@ public class PunchListDates {
         
         LocalDate ts = LocalDate.of(2018, Month.SEPTEMBER, 17);
 
-        Badge b = badgeDAO.find("67637925");
+        Badge b = badgeDAO.find("8C0644BA");
         
         ArrayList<Punch> p1 = punchDAO.list(b, ts);
         
@@ -40,10 +40,10 @@ public class PunchListDates {
         ArrayList<Punch> p2 = new ArrayList<>();
 
         /* Add Punches */
-        p2.add(punchDAO.find(4716));
-        p2.add(punchDAO.find(4811));
-        p2.add(punchDAO.find(4813));
-        p2.add(punchDAO.find(4847));
+        p2.add(punchDAO.find(4659));
+        p2.add(punchDAO.find(4905));
+        p2.add(punchDAO.find(5105));
+        p2.add(punchDAO.find(5182));
         
         for (Punch p : p2) {
             s2.append(p.printOriginal());
@@ -69,6 +69,49 @@ public class PunchListDates {
         LocalDate ts = LocalDate.of(2018, Month.SEPTEMBER, 27);
 
         Badge b = badgeDAO.find("87176FD7");
+
+        
+        ArrayList<Punch> p1 = punchDAO.list(b, ts);
+
+        
+        for (Punch p : p1) {
+            s1.append(p.printOriginal());
+            s1.append("\n");
+        }
+
+        
+        ArrayList<Punch> p2 = new ArrayList<>();
+
+        
+        p2.add(punchDAO.find(6089));
+        p2.add(punchDAO.find(6112));
+        p2.add(punchDAO.find(6118));
+        p2.add(punchDAO.find(6129));
+
+        
+        for (Punch p : p2) {
+            s2.append(p.printOriginal());
+            s2.append("\n");
+        }
+        
+        assertEquals(s2.toString(), s1.toString());
+
+    }
+    
+    @Test
+    public void testFindPunchList3() {
+
+        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+        PunchDAO punchDAO = daoFactory.getPunchDAO();
+
+        
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        
+        LocalDate ts = LocalDate.of(2018, Month.SEPTEMBER, 27);
+
+        Badge b = badgeDAO.find("");
 
         
         ArrayList<Punch> p1 = punchDAO.list(b, ts);
