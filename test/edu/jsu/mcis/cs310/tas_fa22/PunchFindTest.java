156af0404961c4c09663c3bbb648ff3bb079cfe2
garya <garya@10.80.31.48>
2022-12-05 09:50:07 -0600

    Test cases for PunchFindTest 2

--- test/edu/jsu/mcis/cs310/tas_fa22/PunchFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa22/PunchFindTest.java
@@ -102,4 +102,25 @@ public class PunchFindTest {
         
     }
     
+    @Test
+    public void testFindPunches5() {
+
+        PunchDAO punchDAO = daoFactory.getPunchDAO();
+
+        /* Retrieve Punches from Database */
+
+        Punch p20 = punchDAO.find(212);
+        Punch p21 = punchDAO.find(575);
+        Punch p22 = punchDAO.find(1212);
+        Punch p23 = punchDAO.find(1776);
+
+        /* Compare to Expected Values */
+
+        assertEquals("#B09A75D7 TIME OUT: WED 08/01/2018 15:31:49", p20.printOriginal());
+        assertEquals("#76118CDC TIME OUT: MON 08/06/2018 15:33:09", p21.printOriginal());
+        assertEquals("#CB99D1E8 TIME IN: MON 08/13/2018 07:00:25", p22.printOriginal());
+        assertEquals("#BE51FA92 TIME OUT: FRI 08/17/2018 15:32:54", p23.printOriginal());
+    }
+    
+    
 }
